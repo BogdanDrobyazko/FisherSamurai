65d94a38f4cff7272c7802951036c879573b31c0
BogdanDrobyazko <loranjk@yandex.ru>
2024-05-26 17:26:02 +0300

    added experience data

--- Assets/Scripts/GameDataSystem/Data/GameData.cs
+++ Assets/Scripts/GameDataSystem/Data/GameData.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 [Serializable]
 public class GameData
 {
-    public int moneyBalance;
     public bool isGameDataExist;
+    public int experiencePoints;
+    public int moneyBalance;
     public int currentHookIndex;
     public int currentRodIndex;
     public int currentBaitIndex;
 
+    
     //колличество минут прошедших в дне игрового времени, обнуляется каждый игровой день
     public int gameDayTimeInMinutes;
 
